b9337880728186d456bda3b315c4aaeb6955f375
Onodwa2001 <onodwasiyotula7@gmail.com>
2023-09-03 15:38:10 +0200

    Customized graph

--- src/app/admin-dashboard/dashboard/chart/chart.component.ts
+++ src/app/admin-dashboard/dashboard/chart/chart.component.ts
@@ -13,17 +13,16 @@ export class ChartComponent implements OnInit {
     var myChart = new Chart("myChart", {
       type: 'bar',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: '# of Bookings',
+          data: [12, 19, 3, 5, 2, 3, 6, 10, 9, 6, 9, 7],
           backgroundColor: [
-            'rgba(255, 99, 132)',
             'rgba(54, 162, 235)',
-            'rgba(255, 206, 86)',
-            'rgba(75, 192, 192)',
-            'rgba(153, 102, 255)',
-            'rgba(255, 159, 64)'
+            'rgba(54, 162, 235)',
+            'rgba(54, 162, 235)',
+            'rgba(54, 162, 235)',
+            'rgba(54, 162, 235)'
           ],
           borderWidth: 0
         }]
